e5252d3bfd3713cf68e59f53fff6d73e05a66022
klumpra <klumpra@lewisu.edu>
2022-09-02 10:41:09 -0500

    Added a third line of output.

--- SecondExample.java
+++ SecondExample.java
@@ -2,5 +2,6 @@ public class SecondExample {
 	public static void main(String[] args) {
 		System.out.println("Hello. It is Friday.");
 		System.out.println("It is a three-day weekend!");
+		System.out.println("I am looking forward to a day off.");
 	}
 }
